c514637f073d6eb21ec02b91cb2710faabbe5fa1
Benjamin-fillez12 <benjamin.fillez@student-oec.fr>
2024-09-20 11:16:43 +0200

    Update local_time.h

--- local_time.h
+++ local_time.h
@@ -1,8 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <windows.h>
-
-char *get_local_time_buffer(struct tm*);
-void put_local_time_buffer(FILE *,char *, char *);
+"see libs/localTime.c"
