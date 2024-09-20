5fa389842bc2ae894e40049ad82b9d1cdf90fa50
Benjamin-fillez12 <benjamin.fillez@student-oec.fr>
2024-09-20 11:19:38 +0200

    Update truncate.h

--- libs/truncate.h
+++ libs/truncate.h
@@ -1,1 +1,16 @@
 
+/*
+    fichier ref de truncate.h.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+#include <fcntl.h>
+#include <string.h>
+#include <windows.h>
+#include <sys/stat.h>
+
+int change_file_size(FILE *,size_t);
+int resolve_file_err(FILE *,char *);
+size_t get_file_size(char *)
